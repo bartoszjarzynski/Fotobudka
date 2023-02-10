b02ca81fc2fd1d98172278dacb5dc65478d50f53
Jajecznik <mcjajecznik@gmail.com>
2023-02-10 04:13:23 +0100

    converting from activity to fragment

---
+++ app/src/main/java/com/example/fotobudka/CameraFragment.kt
@@ -0,0 +1,162 @@
+package com.example.fotobudka
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.fotobudka.databinding.ActivityMainBinding
+import com.example.fotobudka.databinding.FragmentCameraBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraFragment : Fragment() {
+
+    private var _binding: FragmentCameraBinding? = null
+    private val binding get() = _binding!!
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var activity: Activity
+    private lateinit var externalMediaDirs: Array<File>
+    private lateinit var ctx: Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = requireActivity()
+        ctx = context
+        externalMediaDirs = context.externalMediaDirs
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        if (allPermissionGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                activity, Constants.REQUIRED_PERMISSIONS, Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.optionsBtn.setOnClickListener {
+            //val optionsFragment = OptionsFragment()
+        }
+
+        binding.takePhotoBtn.setOnClickListener {
+            takePhoto()
+        }
+
+        return binding.root
+    }
+
+    private fun getOutputDirectory() : File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+                mFile -> File(mFile, resources.getString(R.string.app_name)).apply {
+            mkdirs()
+            }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else activity.filesDir
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(
+            outputDirectory, SimpleDateFormat(Constants.FILE_NAME_FORMAT,
+                Locale.getDefault()).format(System.currentTimeMillis()) + ".jpg")
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOption, ContextCompat.getMainExecutor(ctx),
+            object: ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "PhotoSaved"
+                    Toast.makeText(ctx, "$msg $savedUri", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG, "onError: ${exception.message}", exception)
+                }
+
+            }
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(ctx, "Permissions not granted", Toast.LENGTH_SHORT).show()
+                activity.finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() =
+        Constants.REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                activity.baseContext, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+    private fun startCamera() {
+        val cameraProviderFeature = ProcessCameraProvider.getInstance(ctx)
+
+        cameraProviderFeature.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFeature.get()
+            val preview = Preview.Builder()
+                .build().also {
+                        mPreview -> mPreview.setSurfaceProvider(
+                    binding.cameraPV.surfaceProvider
+                )
+                }
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+            } catch (e: java.lang.Exception) {
+                Log.d(Constants.TAG, "startCamera Fail: ", e)
+            }
+        }, ContextCompat.getMainExecutor(activity))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+        _binding = null
+    }
+}
